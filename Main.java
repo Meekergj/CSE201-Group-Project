79d201e44d94d06fc9533fc47ea3842bb7a173ce
Baylen Romanello <baylen@waifu.club>
2025-04-04 12:05:36 -0400

    Add guide method

--- Main.java
+++ Main.java
@@ -6,5 +6,17 @@
  *     <p>Purpose:
  */
 public class Main {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    guide();
+  }
+
+  public static void guide() {
+    System.out.println(
+        "Over the next four quarters, you'll face tough choices: risky investments,");
+    System.out.println(
+        "ethical dilemmas, and unexpected crises. Your goal is to make the best decisions");
+    System.out.println(
+        "that will result in the most profit possible. Each decision may have pros and");
+    System.out.println("cons, so choose wisely!");
+  }
 } // end main
