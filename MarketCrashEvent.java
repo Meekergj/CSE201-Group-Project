a4018e41e69815800a25e394b97f3e976ddf41be
Gmann620 <gmeeker622@gmail.com>
2025-04-04 13:50:47 -0400

    commit for scenario 4, classes without internals

---
+++ MarketCrashEvent.java
@@ -0,0 +1,11 @@
+/**
+ * Class: MarketCrashEvent
+ *
+ * @author Gavin Meeker
+ * @version 0.1 Course: CSE 201 Spring 2025 Written: April 4, 2025
+*     <p>Purpose: 
+ */
+
+public class MarketCrashEvent extends Scenario {
+    
+}
---
+++ Scenario.java
@@ -0,0 +1,11 @@
+/**
+ * Class: Scenario
+ *
+ * @author Gavin Meeker
+ * @version 0.1 Course: CSE 201 Spring 2025 Written: April 4, 2025
+*     <p>Purpose: Manages the 4 scenarios/rooms of the text adventure.
+ */
+
+public class Scenario {
+    
+}
