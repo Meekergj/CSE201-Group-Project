b86d103a6673f94f433c38e8e65749aa3818e42c
Braden Gaerke <gaerkebd@miamioh.edu>
2025-04-30 13:22:19 -0400

    work on homelessman event

--- HomelessManEvent.java
+++ HomelessManEvent.java
@@ -11,14 +11,16 @@ public class HomelessManEvent extends Scenario {
      * Give some money to homeless guy
      */
     public void giveMoney() {
-
+        Account.setBalance(Account.getBalance() - (Math.random() * 100); // Assuming a fixed amount of $100 is given
+        System.out.println("You decided to give the homeless man $100.");
     }
 
     /*
      * Fuck it just take what little the homeless guy has
      */
     public void stealFromMan() {
-
+        Account.setBalance(Account.getBalance() + (Math.random() * 100));
+        System.out.println("You stole from the homeless man, and found $100");
     }
     
     /*
