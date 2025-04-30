964e69564bd1e8d60e32b956e183782aa21c2a83
Braden Gaerke <gaerkebd@miamioh.edu>
2025-04-30 14:26:15 -0400

    Finished Stock.java

--- Stock.java
+++ Stock.java
@@ -1,3 +1,76 @@
 public class Stock {
-    
+    private String companyName;
+    private double value;
+    private double changeInValue;
+    private double sharesOwned;
+
+    public Stock(String companyName, double initialValue) {
+        this.companyName = companyName;
+        this.value = initialValue;
+        this.sharesOwned = 0;
+    }
+
+    public Stock(String companyName, double initialValue, double changeInValue, double sharesOwned) {
+        this.companyName = companyName;
+        this.value = initialValue;
+        this.sharesOwned = sharesOwned;
+        this.changeInValue = changeInValue;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public double getValue() {
+        return value;
+    }   
+
+    public double getChangeInValue() {
+        return changeInValue;
+    }
+
+    public double getSharesOwned() {
+        return sharesOwned;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public void setSharesOwned(double sharesOwned) {
+        this.sharesOwned = sharesOwned;
+    }
+
+    // Derived value
+    public double calculateAmountInvested() {
+        return sharesOwned * value;
+    }
+
+    public double calculateChangeInValue(double initialValue, double currentValue) {
+        return (currentValue - initialValue) / initialValue;
+    }
+
+    public void displayStock() {
+        System.out.printf("%s - Value: $%.2f | Shares: %.2f | Invested: $%.2f | Δ: %.2f%%\n",
+                companyName, value, sharesOwned, calculateAmountInvested(), changeInValue * 100);
+    }
+
+    public double buyShares(double numShares) {
+        double cost = numShares * value;
+        sharesOwned += numShares;
+        return cost;
+    }
+
+    // Sell shares: returns the total revenue or -1 if not enough shares
+    public double sellShares(double numShares) {
+        if (numShares > sharesOwned) {
+            System.out.println("Not enough shares to sell.");
+            return -1;
+        }
+        double revenue = numShares * value;
+        sharesOwned -= numShares;
+        return revenue;
+    }
+
+
 }
