8c7d43b695d7a39bd102b553cc967915fbd23f85
Baylen Romanello <baylen@waifu.club>
2025-04-30 14:14:58 -0400

    Add start method

--- ScenarioThree.java
+++ ScenarioThree.java
@@ -1,6 +1,15 @@
 public class ScenarioThree {
   public void start() {
     int number = (int) (Math.random() * 3) + 1; // Generates 1, 2, or 3
+    if (number == 1) {
+      batemanEvent();
+    }
+    if (number == 2) {
+      carCrash();
+    }
+    if (number == 3) {
+      bullishTrend();
+    }
   }
 
   public void batemanEvent() {
